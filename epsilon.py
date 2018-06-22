8e0a58b551de41c59855a01cc62adbdc1ecadac6
Gabriel Schneider <gabriel.schneider.almeida@gmail.com>
2018-06-21 22:53:16 -0300

    Remove old epsilon modules

--- epsilon.py
+++
@@ -1,8 +0,0 @@
-"""Module for epsilon functions."""
-
-# Please dont remove this defalt function
-def default(cycle, max_cycle, environment):
-    """Return epsilon value for a cycle."""
-    return 0.01 - (0.00985 * min(cycle, 75) / 75)
-
-# Add your epsilon functions here!
